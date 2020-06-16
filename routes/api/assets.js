b3453622ae67d05570cbe490c6667acd07eb759b
J1-js <shinjayw@gmail.com>
2020-06-16 19:53:41 -0400

    More asset route development

--- routes/api/assets.js
+++ routes/api/assets.js
@@ -1,13 +1,22 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const Asset = require('../../models/Asset')
+const Asset = require('../../models/Asset');
 
+// Retrieve all assets
 router.get('/', (req, res) => {
     Asset.find()
-    .then(assets => res.json(assets))
+    .then(assets => {res.json(assets)})
     .catch(err => {res.json({message: err})})
 })
 
+// Retrieve specific asset by id
+router.get('/:assetId', (req, res) => {
+    Asset.findById(req.params.assetId)
+    .then(assets => {res.json(assets)})
+    .catch(err => {res.json({message: err})})
+})
+
+// Generates new asset
 router.post('/', (req, res) => {
     const asset = new Asset({
         assetName: req.body.assetName,
@@ -18,7 +27,8 @@ router.post('/', (req, res) => {
     asset.save()
         .then(data => {res.json(data)})
         .catch(err => {res.json({message: err})})
-})
+});
+
 
 
 module.exports = router;
