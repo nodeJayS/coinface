8790a09b499048176d077f22d034b08740b8c53c
J1-js <shinjayw@gmail.com>
2020-07-18 21:20:47 -0400

    added static site

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+  
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
+
 // Middleware configuration
 app.use(express.json());
 app.use(passport.initialize());
