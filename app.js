f5e1c8c70fb7c55df03cb0f00fc9bcbf98a004c6
J1-js <shinjayw@gmail.com>
2020-07-20 14:49:47 -0400

    Installed bodyparser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const passport = require('passport');
 const cors = require('cors');
+var bodyParser = require('body-parser')
 const path = require('path')
 
 const users = require('./routes/api/users');
@@ -18,6 +19,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+// Middleware configuration
+app.use(express.json());
+app.use(passport.initialize());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+app.use('/api/transactions', transactions);
+app.use('/api/assets', assets);
+app.use('/api/watchlist', watchlist)
+
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
@@ -25,16 +38,6 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   });
 }
-
-// Middleware configuration
-app.use(express.json());
-app.use(passport.initialize());
-app.use(cors());
-
-app.use('/api/users', users);
-app.use('/api/transactions', transactions);
-app.use('/api/assets', assets);
-app.use('/api/watchlist', watchlist)
 
 const port = process.env.PORT || 5000;
 
