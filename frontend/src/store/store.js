d081f62abbc06a6bc725d89ff8ad0a83802b10b3
J1-js <shinjayw@gmail.com>
2020-07-20 14:22:52 -0400

    disable devtools

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,21 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../reducers/rootReducer'
+
+// const configureStore = (preloadedState = {}) => (createStore(
+//     rootReducer,
+//     preloadedState, 
+//     composeWithDevTools(
+//     applyMiddleware(thunk, logger))
+//     ));
 
 const configureStore = (preloadedState = {}) => (createStore(
     rootReducer,
     preloadedState, 
-    composeWithDevTools(
     applyMiddleware(thunk, logger))
-    ));
+    );
 
 export default configureStore
