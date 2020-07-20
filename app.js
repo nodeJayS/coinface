e68e8a85e28bd8cd5c99777ae3ca3483d86e7255
J1-js <shinjayw@gmail.com>
2020-07-20 12:35:48 -0400

    Test connection

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static('frontend/build'));
   
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-  }
+  //   const path = require('path');
+  //   app.get('*', (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  //   });
+  // }
 
 // Middleware configuration
 app.use(express.json());
